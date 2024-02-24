6b1a361d8ba30cd33dca70ad51fb3bb97642b75e
David Bisrat <>
2024-02-23 23:42:34 -0500

    add supabase subscription hook

--- src/components/rewards-card/rewards-card.tsx
+++ src/components/rewards-card/rewards-card.tsx
@@ -7,7 +7,8 @@ import { Tables } from "@PNN/utils/data-access/database.types";
 import PunchNode from "./punch-node";
 import { Button } from "@PNN/components/ui/button";
 import { redeemRewards } from "@PNN/utils/data-access/data-acess";
-import { createClient } from "@PNN/utils/supabase/client";
+import { Skeleton } from "../ui/skeleton";
+import { useSupabaseRealtimeSubscription } from "@PNN/hooks/supabase-real-time-subscription";
 interface RewardsCardProps {
   card: Tables<"reward_card">;
   maxPoints: number;
@@ -22,36 +23,21 @@ const RewardsCard: React.FC<PropsWithChildren<RewardsCardProps>> = ({
 }) => {
   const [isFlipped, setIsFlipped] = useState(false);
   const [isAnimating, setIsAnimating] = useState(false);
+  const [isLoading, setLoading] = useState(true);
   const [points, setPoints] = useState(card.points);
   const [ignorePunchUpdate, setIgnorePunchUpdate] = useState(false);
+  const { isReady } = useSupabaseRealtimeSubscription(
+    (payload) => setPoints(payload.new.points),
+    "UPDATE",
+    "reward_card",
+    `id=eq.${card.id}`
+  );
 
   useEffect(() => {
-    let sub = createClient()
-      .channel("card_update")
-      .on(
-        "postgres_changes",
-        {
-          event: "UPDATE",
-          schema: "public",
-          table: "reward_card",
-          filter: "id=eq." + card.id,
-        },
-        (payload) => {
-          const updatedCard = payload.new;
-          setPoints(updatedCard.points);
-        }
-      )
-      .subscribe((connection) => {
-        console.log("connection", connection);
-        if (connection === "SUBSCRIBED") {
-          console.log("Subscribed to changes");
-        }
-      });
-
-    return () => {
-      sub.unsubscribe();
-    };
-  }, []);
+    if (isReady) {
+      setLoading(false);
+    }
+  }, [isReady]);
 
   function handleFlip() {
     if (!isAnimating) {
@@ -61,70 +47,85 @@ const RewardsCard: React.FC<PropsWithChildren<RewardsCardProps>> = ({
   }
 
   return (
-    <div className="flex flex-col gap-3 h-full items-center justify-center cursor-pointer">
-      <div
-        className="flip-card  w-[375px] h-[225px] rounded-md"
-        onClick={handleFlip}
-      >
-        <motion.div
-          className="flip-card-inner h-full"
-          initial={false}
-          animate={{ rotateY: isFlipped ? 180 : 360 }}
-          transition={{ duration: 0.3, animationDirection: "normal" }}
-          onAnimationComplete={() => setIsAnimating(false)}
-        >
-          <div
-            className="flip-card-front w-full h-full bg-cover border-[1px] text-white rounded-lg p-4"
-            style={{
-              backgroundImage: `url(${front.src})`,
-              backgroundSize: "100% 100%",
-              backgroundRepeat: "no-repeat",
-            }}
-          ></div>
-
-          <div
-            className="flip-card-back w-full h-full bg-cover border-[1px] text-white rounded-lg"
-            style={{
-              backgroundImage: `url(${back.src})`,
-              backgroundSize: "100% 100%",
-              backgroundRepeat: "no-repeat",
-            }}
-          >
-            <div className="absolute top-1 left-2">{points}</div>
-            <div className="grid grid-cols-6 h-full p-4">
-              <div className="justify-self-center row-start-2 col-start-2 col-span-4 row-span-3">
-                {children}
-              </div>
-              {Array(maxPoints)
-                .fill(0)
-                .map((_, i) => {
-                  let className = "justify-self-center";
-                  if (i == 0 || i === 5) {
-                    className = `${className} row-span-2 self-center`;
-                  }
-
-                  return (
-                    <div key={i} className={className}>
-                      <PunchNode
-                        cardId={card.id}
-                        total={points}
-                        punched={i < points}
-                        ignorePunchUpdate={ignorePunchUpdate}
-                        setIgnorePunchUpdate={setIgnorePunchUpdate}
-                      ></PunchNode>
-                    </div>
-                  );
-                })}
-            </div>
+    <>
+      {isLoading && (
+        <div className="flex flex-col space-y-3 justify-center items-center">
+          <Skeleton className="w-[375px] h-[225px] rounded-md" />
+          <div className="space-y-2">
+            <Skeleton className="h-4 w-[250px]" />
+            <Skeleton className="h-4 w-[200px]" />
           </div>
-        </motion.div>
-      </div>
-      {maxPoints === points && canModify && (
-        <div>
-          <Button onClick={() => redeemRewards(card.id)}>Redeem Points</Button>
         </div>
       )}
-    </div>
+      {!isLoading && (
+        <div className="flex flex-col gap-3 h-full items-center justify-center cursor-pointer">
+          <div
+            className="flip-card  w-[375px] h-[225px] rounded-md"
+            onClick={handleFlip}
+          >
+            <motion.div
+              className="flip-card-inner h-full"
+              initial={false}
+              animate={{ rotateY: isFlipped ? 180 : 360 }}
+              transition={{ duration: 0.3, animationDirection: "normal" }}
+              onAnimationComplete={() => setIsAnimating(false)}
+            >
+              <div
+                className="flip-card-front w-full h-full bg-cover border-[1px] text-white rounded-lg p-4"
+                style={{
+                  backgroundImage: `url(${front.src})`,
+                  backgroundSize: "100% 100%",
+                  backgroundRepeat: "no-repeat",
+                }}
+              ></div>
+
+              <div
+                className="flip-card-back w-full h-full bg-cover border-[1px] text-white rounded-lg"
+                style={{
+                  backgroundImage: `url(${back.src})`,
+                  backgroundSize: "100% 100%",
+                  backgroundRepeat: "no-repeat",
+                }}
+              >
+                <div className="absolute top-1 left-2">{points}</div>
+                <div className="grid grid-cols-6 h-full p-4">
+                  <div className="justify-self-center row-start-2 col-start-2 col-span-4 row-span-3">
+                    {children}
+                  </div>
+                  {Array(maxPoints)
+                    .fill(0)
+                    .map((_, i) => {
+                      let className = "justify-self-center";
+                      if (i == 0 || i === 5) {
+                        className = `${className} row-span-2 self-center`;
+                      }
+
+                      return (
+                        <div key={i} className={className}>
+                          <PunchNode
+                            cardId={card.id}
+                            total={points}
+                            punched={i < points}
+                            ignorePunchUpdate={ignorePunchUpdate}
+                            setIgnorePunchUpdate={setIgnorePunchUpdate}
+                          ></PunchNode>
+                        </div>
+                      );
+                    })}
+                </div>
+              </div>
+            </motion.div>
+          </div>
+          {maxPoints === points && canModify && (
+            <div>
+              <Button onClick={() => redeemRewards(card.id)}>
+                Redeem Points
+              </Button>
+            </div>
+          )}
+        </div>
+      )}
+    </>
   );
 };
 
---
+++ src/hooks/supabase-real-time-subscription.ts
@@ -0,0 +1,56 @@
+import { createClient } from "@PNN/utils/supabase/client";
+import {
+  REALTIME_LISTEN_TYPES,
+  RealtimePostgresChangesFilter,
+  RealtimePostgresChangesPayload,
+} from "@supabase/supabase-js";
+import { useEffect, useState } from "react";
+
+const defaultCB = (payload: any) => {
+  console.log(["Received realtime event", payload]);
+};
+
+export function useSupabaseRealtimeSubscription(
+  callback: (param: RealtimePostgresChangesPayload<any>) => void = defaultCB,
+  event: string = "*",
+  table: string,
+  filter: string
+) {
+  const supabaseClient = createClient();
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    let initialMessageSkipped = false;
+
+    const channel = supabaseClient
+      .channel(table === "*" ? "public" : `public:${table}`)
+      .on(
+        REALTIME_LISTEN_TYPES.POSTGRES_CHANGES,
+        {
+          schema: "public",
+          table,
+          event,
+          filter,
+        } as RealtimePostgresChangesFilter<"*">,
+        (payload) => {
+          callback(payload);
+        }
+      )
+      .on("system" as any, {} as any, (payload: any) => {
+        if (payload.extension == "postgres_changes" && payload.status == "ok") {
+          if (!initialMessageSkipped) {
+            initialMessageSkipped = true;
+            return;
+          }
+          setIsReady(true);
+        }
+      })
+      .subscribe();
+
+    return () => {
+      supabaseClient.removeChannel(channel);
+    };
+  }, []);
+
+  return { isReady };
+}
