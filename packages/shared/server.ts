4d7f68a14cc23677ff87165aaf5d44e3f5a40698
David <dabisrat@gmail.com>
2025-06-21 00:52:20 -0400

    add dashboard app

--- packages/shared/server.ts
+++ packages/shared/server.ts
@@ -1,9 +1,8 @@
 import { createServerClient, type CookieOptions } from '@supabase/ssr'
-import { cookies } from 'next/headers'
+import type { cookies } from 'next/headers'
 import { Database } from '../../apps/equivalent-exchange/src/utils/database.types'
 
 export const createClient = (cookieStore: ReturnType<typeof cookies>) => {
-  // make sure this is a sigleton
   return createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
